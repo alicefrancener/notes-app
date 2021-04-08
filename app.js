077c4ecac84038d67629a7fc5cdda2bf9ee3c14c
Alice Francener <alicefrancener@gmail.com>
2021-04-08 11:45:33 -0300

    feat: add options to add command

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const yargs = require("yargs");
 
 yargs
-  .command("add", "Add a new note", () => console.log("Adding a new note..."))
+  .command(
+    "add",
+    "Add a new note",
+    (yargs) => {
+      yargs
+        .option("title", {
+          describe: "Note title",
+          string: true,
+          demandOption: true,
+        })
+        .option("body", {
+          describe: "Note body",
+          string: true,
+          demandOption: true,
+        });
+    },
+    (argv) => console.log(`Title: ${argv.title}\nBody: ${argv.body}`)
+  )
   .command("remove", "Remove a note", () => console.log("Removing a note..."))
   .command("list", "List all notes", () => console.log("Listing all notes..."))
   .command("read", "Read a specific note", () =>
