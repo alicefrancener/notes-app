541c7043d921e4b4871c3df888ae7732f1ed1a3a
Alice Francener <alicefrancener@gmail.com>
2021-04-09 16:47:16 -0300

    refactor: functions to arrow function

--- notes.js
+++ notes.js
@@ -1,7 +1,7 @@
 const chalk = require("chalk");
 const fs = require("fs");
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
   const notes = loadNotes();
   const duplicateNotes = notes.filter((note) => note.title === title);
 
@@ -17,7 +17,7 @@ const addNote = function (title, body) {
   }
 };
 
-const removeNote = function (title) {
+const removeNote = (title) => {
   const notes = loadNotes();
   const notesToKeep = notes.filter((note) => note.title != title);
 
@@ -29,12 +29,12 @@ const removeNote = function (title) {
   }
 };
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync("notes.json", dataJSON);
 };
 
-const loadNotes = function () {
+const loadNotes = () => {
   try {
     const dataBuffer = fs.readFileSync("notes.json");
     const dataJSON = dataBuffer.toString();
