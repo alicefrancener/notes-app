8f2f6daac83b512898cf172b9653ff1f13bc6ede
Alice Francener <alicefrancener@gmail.com>
2021-04-10 22:19:33 -0300

    refactor: filter to find method

--- notes.js
+++ notes.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 
 const addNote = (title, body) => {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter((note) => note.title === title);
+  const duplicateNote = notes.find((note) => note.title === title);
 
-  if (duplicateNotes.length === 0) {
+  if (!duplicateNote) {
     notes.push({
       title,
       body,
